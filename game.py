9f4effdb621d8e696ae377500abfc94c71ea7025
Alex Jonker <alex.jonker2008@gmail.com>
2025-01-08 16:30:59 +0100

    fix: ^C in entire script fix

--- game.py
+++ game.py
@@ -9,21 +9,21 @@ cars = {
 }
 
 
-def load():
-    if not os.path.exists("./data.json"):
-        json.dump({}, open("./data.json", "w"))
-    data = json.load(open("./data.json", "r"))
-    return data
+try:
+    def load():
+        if not os.path.exists("./data.json"):
+            json.dump({}, open("./data.json", "w"))
+        data = json.load(open("./data.json", "r"))
+        return data
 
 
-def add(name, value):
-    table = load()
-    table[name] = value
-    json.dump(table, open("./data.json", "w"))
+    def add(name, value):
+        table = load()
+        table[name] = value
+        json.dump(table, open("./data.json", "w"))
 
 
-def ask(options):
-    try:
+    def ask(options):
         for i, option in enumerate(options, start=1):
             print(f"{i}. {option}")
         answer = input("Please enter the number of your choice: ")
@@ -32,62 +32,65 @@ def ask(options):
         else:
             print("Invalid choice, please try again.")
             return ask(options)
-    except KeyboardInterrupt:
         print("")
         print("Goodbye!")
         exit()
 
 
-def new_player():
-    print("Welcome! Looks like this is your first time playing.")
-    print("What's your name?")
-    add("name", input("Name: "))
-    print(f"Nice to meet you, {load()['name']}!")
-    sleep(1)
-    print("Please choose your starter car:")
-    add("car", ask(["1990 Mazda Miata (116 HP)", "1983 Toyota AE86 (112 HP", "1987 Suzuki Swift GTi (100 HP)",]))
-    add("hp", cars[int(load()["car"])]["hp"])
-    print(f"You chose the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']}, Good choice!")
-    sleep(1)
-    print("Here's $3K to get started!")
-    add("money", 3000)
-    print("Have fun playing!")
-    # adding some default values to the data.json file
-    add("level", 1)
-    add("xp", 0)
-    sleep(2)
+    def new_player():
+        print("Welcome! Looks like this is your first time playing.")
+        print("What's your name?")
+        add("name", input("Name: "))
+        print(f"Nice to meet you, {load()['name']}!")
+        sleep(1)
+        print("Please choose your starter car:")
+        add("car", ask(["1990 Mazda Miata (116 HP)", "1983 Toyota AE86 (112 HP", "1987 Suzuki Swift GTi (100 HP)",]))
+        add("hp", cars[int(load()["car"])]["hp"])
+        print(f"You chose the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']}, Good choice!")
+        sleep(1)
+        print("Here's $3K to get started!")
+        add("money", 3000)
+        print("Have fun playing!")
+        # adding some default values to the data.json file
+        add("level", 1)
+        add("xp", 0)
+        sleep(2)
 
 
-#################################
+    #################################
 
 
-def race():
-    print("You chose racing!")
-    return
+    def race():
+        print("You chose racing!")
+        return
 
-def upgrade():
-    print("You chose upgrading/repairing your car!")
-    return
+    def upgrade():
+        print("You chose upgrading/repairing your car!")
+        return
 
 
-def start():
-    if load() == {}:
-        new_player()
-    else:
-        print(f"Welcome back! Your current car is the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']} and you are pushing {load()["hp"]} HP.")
+    def start():
+        if load() == {}:
+            new_player()
+        else:
+            print(f"Welcome back! Your current car is the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']} and you are pushing {load()["hp"]} HP.")
 
-    print("---------------------------------------")
-    print("What do you want to do?")
-    choice = ask(["Race", "Upgrade/repair your car", "Exit"])
-    if choice == 1:
-        race()
-    elif choice == 2:
-        upgrade()
-    elif choice == 3:
-        print("Goodbye!")
-        exit()
+        print("---------------------------------------")
+        print("What do you want to do?")
+        choice = ask(["Race", "Upgrade/repair your car", "Exit"])
+        if choice == 1:
+            race()
+        elif choice == 2:
+            upgrade()
+        elif choice == 3:
+            print("Goodbye!")
+            exit()
+
+        start()
+
 
     start()
-
-
-start()
+except KeyboardInterrupt:
+    print("")
+    print("Goodbye!")
+    exit()

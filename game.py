57e2fd391f90cff1b39513c792b6575d440ac61d
Alex Jonker <alex.jonker2008@gmail.com>
2025-01-08 13:23:13 +0100

    feat: added new_player and a check to detect new players

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import json
 import os
+from time import *
 
 cars = {
     1: {
@@ -32,3 +33,16 @@ def add(name, value):
     table = load()
     table[name] = value
     json.dump(table, open('./data.json', 'w'))
+
+
+def new_player():
+    print("Please choose your starter car:")
+    print("1. 1990 Mazda Miata (116 HP)")
+    print("2. 1983 Toyota AE86 (112 HP)")
+    print("3. 1987 Suzuki Swift GTi (100 HP)")
+    add("car", input("1, 2 or 3: "))
+
+if load() == {}:
+    new_player()
+else:
+    print(f"Welcome back! Your current car is the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']} and you are pushing {cars[int(load()['car'])]['hp']} HP.")

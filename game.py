77fdba22c015980d620f379e452d6be94cf39d2d
Alex Jonker <alex.jonker2008@gmail.com>
2025-01-10 20:29:14 +0100

    feat: klaar met het toevoegen van curses

--- game.py
+++ game.py
@@ -9,8 +9,7 @@ stdscr.refresh()
 curses.start_color()
 curses.init_pair(1, 1, 233) # selectie en achtergrond kleuren
 stdscr.bkgd(curses.color_pair(1))
-curses.curs_set(0) # balkje wanneer je typt weg
-
+curses.curs_set(0) # hide the little type indicator
 cars = {
     1: {"name": "Miata", "brand": "Mazda", "year": 1990, "hp": 116},
     2: {"name": "AE86", "brand": "Toyota", "year": 1983, "hp": 112},
@@ -29,6 +28,7 @@ try:
         x = (width // 2) - (len(text) // 2)
         stdscr.addstr(y, x, f"{text} \n")
         stdscr.refresh()
+
     def load():
         if not os.path.exists("./data.json"):
             json.dump({}, open("./data.json", "w"))
@@ -42,31 +42,72 @@ try:
         json.dump(table, open("./data.json", "w"))
 
 
-    def ask(options):
-        for i, option in enumerate(options, start=1):
-            print(f"{i}. {option}")
-        answer = input("Please enter the number of your choice: ")
-        if (answer.isnumeric()) and (int(answer) in range(1, len(options) + 1)):
-            return int(answer)
-        else:
-            print("Invalid choice, please try again.")
-            return ask(options)
+
+    def ask(text, options):
+        def main(stdscr):
+            current_selection = 0
+            count = len(options)
+
+            while True:
+                clear()
+                height, width = stdscr.getmaxyx()
+
+                display(text)
+
+                for num, option in enumerate(options):
+                    y = 2 + num
+                    x = (width // 2) - (len(option) // 2)
+                    if num == current_selection:
+                        stdscr.addstr(y, x, f"> {option}", curses.A_REVERSE)
+                    else:
+                        stdscr.addstr(y, x, f"  {option}")
+
+                stdscr.refresh()
+
+                key = stdscr.getch()
+                if key == curses.KEY_UP:
+                    current_selection = (current_selection - 1) % count
+                elif key == curses.KEY_DOWN:
+                    current_selection = (current_selection + 1) % count
+                elif key in [curses.KEY_ENTER, ord("\n")]:
+                    return current_selection + 1
+
+        return curses.wrapper(main)
+
+
+
 
 
     def new_player():
-        print("Welcome! Looks like this is your first time playing.")
-        print("What's your name?")
-        add("name", input("Name: "))
-        print(f"Nice to meet you, {load()['name']}!")
+        display("Welcome! Looks like this is your first time playing.")
+
+        curses.curs_set(1)
+        curses.echo()
+        height, width = stdscr.getmaxyx()
+        prompt = "What's your name? "
+        y, _ = stdscr.getyx()
+        x = (width // 2) - (len(prompt) // 2)
+
+        stdscr.addstr(y, x, prompt)
+        input_x = x + len(prompt)
+        stdscr.move(y, input_x)
+        stdscr.refresh()
+
+        player_name = stdscr.getstr().decode('utf-8')
+        curses.noecho()
+        curses.curs_set(0)
+
+        add("name", player_name)
+        display(f"Nice to meet you, {load()['name']}!")
         sleep(1)
-        print("Please choose your starter car:")
-        add("car", ask(["1990 Mazda Miata (116 HP)", "1983 Toyota AE86 (112 HP", "1987 Suzuki Swift GTi (100 HP)",]))
+        add("car", ask("Please choose your starter car:", ["1990 Mazda Miata (116 HP)", "1983 Toyota AE86 (112 HP", "1987 Suzuki Swift GTi (100 HP)",]))
         add("hp", cars[int(load()["car"])]["hp"])
-        print(f"You chose the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']}, Good choice!")
+        clear()
+        display(f"You chose the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']}, Good choice!")
         sleep(1)
-        print("Here's $3K to get started!")
+        display("Here's $3K to get started!")
         add("money", 3000)
-        print("Have fun playing!")
+        display("Have fun playing!")
         # adding some default values to the data.json file
         add("level", 1)
         add("xp", 0)
@@ -77,11 +118,13 @@ try:
 
 
     def race():
-        print("You chose racing!")
+        clear()
+        display("You chose racing!")
         return
 
     def upgrade():
-        print("You chose upgrading/repairing your car!")
+        clear()
+        display("You chose upgrading/repairing your car!")
         return
 
 
@@ -89,17 +132,16 @@ try:
         if load() == {}:
             new_player()
         else:
-            print(f"Welcome back! Your current car is the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']} and you are pushing {load()["hp"]} HP.")
-
-        print("---------------------------------------")
-        print("What do you want to do?")
-        choice = ask(["Race", "Upgrade/repair your car", "Exit"])
+            display(f"Welcome back! Your current car is the {cars[int(load()['car'])]['year']} {cars[int(load()['car'])]['brand']} {cars[int(load()['car'])]['name']} and you are pushing {load()["hp"]} HP.")
+            display(f"You are currently at level {load()['level']} with {load()['xp']} XP.")
+        choice = ask("What do you want to do?", ["Race", "Upgrade/repair your car", "Exit"])
         if choice == 1:
             race()
         elif choice == 2:
             upgrade()
         elif choice == 3:
-            print("Goodbye!")
+            clear()
+            display("Goodbye!")
             exit()
 
         start()
@@ -107,6 +149,4 @@ try:
 
     start()
 except KeyboardInterrupt:
-    print("")
-    print("Goodbye!")
     exit()
